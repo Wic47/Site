b30ffd59b90d0ea3d2fda29dd47f261d88851e75
Wic47 <wilhelm.carlsson@skola.taby.se>
2023-05-15 22:25:28 +0200

    :)

--- front-end/funktioner.js
+++ front-end/funktioner.js
@@ -1,17 +1,21 @@
-let collapse = document.getElementById("collapse");
-let aside = document.querySelector("aside");
-let aside2 = document.querySelector(".aside2");
-let svar = document.getElementById("chat");
-let info = document.querySelectorAll(".container-container");
-let ex = document.getElementsByClassName("example");
-let modes = document.getElementById("modes");
-let r = document.querySelector(":root");
-let purchase = document.getElementById("purchase");
-let upgrade = document.getElementById("upgrade");
-let textc = document.querySelector(".text-container");
+const collapse = document.getElementById("collapse");
+const aside = document.querySelector("aside");
+const aside2 = document.querySelector(".aside2");
+const svar = document.getElementById("chat");
+const info = document.querySelectorAll(".container-container");
+const ex = document.getElementsByClassName("example");
+const modes = document.querySelectorAll(".modes");
+const r = document.querySelector(":root");
+const purchase = document.getElementById("purchase");
+const upgrade = document.querySelectorAll(".upgrade");
+const textc = document.querySelector(".text-container");
 const closemenu = document.querySelector(".close");
 const burgercontainer = document.querySelector(".borgir-container");
 const burgermenu = document.getElementById("burger");
+const slide_animation = document.querySelector(".slide-animation");
+const fade_animation = document.querySelector(".fade-animation");
+const blur = document.querySelector(".blur");
+
 for (let i = 0; i < ex.length; i++) {
   ex[i].addEventListener("click", function () {
     svar.value = ex[i].innerHTML.replaceAll(`"`, "").replace(` →`, "");
@@ -25,55 +29,68 @@ collapse2.addEventListener("click", function () {
   aside.style.display = "flex";
   aside2.style.display = "none";
 });
-upgrade.addEventListener("click", function () {
-  if (purchase.style.display === "flex") {
-    purchase.style.display = "none";
-  } else {
-    purchase.style.display = "flex";
-  }
+upgrade.forEach((e) => {
+  e.addEventListener("click", function () {
+    if (purchase.style.display === "flex") {
+      purchase.style.display = "none";
+    } else {
+      purchase.style.display = "flex";
+    }
+  });
 });
 closemenu.addEventListener("click", function () {
-  burgercontainer.classList.add("slide-out");
-  burgercontainer.classList.remove("slide-in");
+  slide_animation.style.setProperty("animation-name", "slide-out");
+  fade_animation.style.setProperty("animation-name", "fade-out");
 });
 burgermenu.addEventListener("click", function () {
   burgercontainer.style.display = "flex";
-  burgercontainer.classList.add("slide-in");
-  burgercontainer.classList.remove("slide-out");
+  blur.style.display = "flex";
+  slide_animation.style.setProperty("animation-name", "slide-in");
+  fade_animation.style.setProperty("animation-name", "fade-in");
+});
+let animationtoggle = false;
+slide_animation.addEventListener("animationend", function () {
+  if (animationtoggle) {
+    burgercontainer.style.display = "none";
+    blur.style.display = "none";
+  }
+  animationtoggle = !animationtoggle;
 });
 
-let i = false;
-modes.addEventListener("click", function () {
-  i = !i;
-  if (i) {
-    r.style.setProperty("--bg", "#EEEEEE");
-    r.style.setProperty("--chat", "#A3A3A3");
-    r.style.setProperty("--info", "#D6D6D6");
-    r.style.setProperty("--info-hover", "#B8B8B8");
-    r.style.setProperty("--aside", "#BBBBBB");
-    r.style.setProperty("--account", "#AAAAAA");
-    r.style.setProperty("--account-hover", "#999999");
-    r.style.setProperty("--hover", "#858585");
-    r.style.setProperty("--new", "#ADADAD");
-    r.style.setProperty("--color", "black");
-    r.style.setProperty("--chat-color", "#141414");
-    r.style.setProperty("--chat-hover", "#858585");
-    r.style.setProperty("--text", "#dee2e6");
-    modes.innerHTML = `<i id="mode" class="fa-solid fa-moon"></i> Dark mode`;
-  } else {
-    r.style.setProperty("--bg", "#434C5E");
-    r.style.setProperty("--chat", "#1234");
-    r.style.setProperty("--info", "#333A47");
-    r.style.setProperty("--info-hover", "#22272F");
-    r.style.setProperty("--aside", "#2B303B");
-    r.style.setProperty("--account", "#252323");
-    r.style.setProperty("--account-hover", "#151414");
-    r.style.setProperty("--hover", "#3C4453");
-    r.style.setProperty("--new", "#99A3AD");
-    r.style.setProperty("--color", "white");
-    r.style.setProperty("--chat-color", "#D2D6DB");
-    r.style.setProperty("--chat-hover", "#1234");
-    r.style.setProperty("--text", "#3123");
-    modes.innerHTML = `<i id="mode" class="fa-solid fa-sun"></i> Light mode`;
-  }
+let mode = false;
+modes.forEach((e) => {
+  e.addEventListener("click", function () {
+    mode = !mode;
+    if (mode) {
+      r.style.setProperty("--bg", "#EEEEEE");
+      r.style.setProperty("--chat", "#A3A3A3");
+      r.style.setProperty("--info", "#D6D6D6");
+      r.style.setProperty("--info-hover", "#B8B8B8");
+      r.style.setProperty("--aside", "#BBBBBB");
+      r.style.setProperty("--account", "#AAAAAA");
+      r.style.setProperty("--account-hover", "#999999");
+      r.style.setProperty("--hover", "#858585");
+      r.style.setProperty("--new", "#ADADAD");
+      r.style.setProperty("--color", "black");
+      r.style.setProperty("--chat-color", "#141414");
+      r.style.setProperty("--chat-hover", "#858585");
+      r.style.setProperty("--text", "#dee2e6");
+      modes.innerHTML = `<i id="mode" class="fa-solid fa-moon"></i> Dark mode`;
+    } else {
+      r.style.setProperty("--bg", "#434C5E");
+      r.style.setProperty("--chat", "#1234");
+      r.style.setProperty("--info", "#333A47");
+      r.style.setProperty("--info-hover", "#22272F");
+      r.style.setProperty("--aside", "#2B303B");
+      r.style.setProperty("--account", "#252323");
+      r.style.setProperty("--account-hover", "#151414");
+      r.style.setProperty("--hover", "#3C4453");
+      r.style.setProperty("--new", "#99A3AD");
+      r.style.setProperty("--color", "white");
+      r.style.setProperty("--chat-color", "#D2D6DB");
+      r.style.setProperty("--chat-hover", "#1234");
+      r.style.setProperty("--text", "#3123");
+      modes.innerHTML = `<i id="mode" class="fa-solid fa-sun"></i> Light mode`;
+    }
+  });
 });
--- server/server.js
+++ server/server.js
@@ -9,35 +9,39 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = 5000
+const port = 5000;
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
-app.post('/', async (req,res) => {
+app.post("/", async (req, res) => {
+  const { messages } = req.body;
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: `You are Chat GPT a helpful assistant designed to help with anything.`,
+        },
+        ...messages,
+      ],
+      temperature: 0.4,
+      max_tokens: 2048,
+      frequency_penalty: 0.5,
+    });
 
-    const { messages } = req.body;
-    try {   
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {role: "system", content: `You are Chat GPT a helpful assistant designed to help with anything.`},
-                ...messages
-            ],
-            temperature: 0.4,
-            max_tokens: 2048,
-            frequency_penalty: 0.5,
-            });
+    res.json({
+      completion: completion.data.choices[0].message,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        res.json({
-            completion: completion.data.choices[0].message
-        })
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.listen(port, () => console.log(`Servern körs nu på port http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Servern körs nu på port http://localhost:${port}`)
+);
